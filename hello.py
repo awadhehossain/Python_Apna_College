b50486394cbfe4c7b14c79111aaf5dc20fbf55e6
Awadhe Hossain <awadhehossain@Awadhes-MacBook-Pro-4.local>
2024-11-01 09:08:42 +0600

    new

---
+++ hello.py
@@ -0,0 +1,11 @@
+#print function simply print what user input or Between the parentesis
+print("Hello World!")
+print("Hello Visitor")
+print("This is my first python programme")
+
+#If we use multiple print statement this line would be print in muliple line .
+#If we want to print multiple statement in 1 line .we have to use comma 
+
+print("Hello WOrld!","My name is Awadhe.","My drem is to explore the world")
+# ,Comma take a space automatically after the first string end point
+print("H","e","l","l","0")
